51b5689ffbbd40ec380fdc1ef25f15de4ee4a2b5
Benjamin Lannon <lannonbr@clarkson.edu>
2016-05-30 11:18:10 -0400

    Refactoring CSS

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp'),
-		jshint = require('gulp-jshint');
+	jshint = require('gulp-jshint'),
+	sass = require('gulp-sass');
 
-gulp.task('lint', function() {
+gulp.task('lint', () => {
 	return gulp.src('./index.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('jshint-stylish'));
 });
+
+gulp.task('sass', () => {
+	return gulp.src("./sass/app.scss")
+		.pipe(sass({outputStyle: 'expanded'}))
+		.pipe(gulp.dest("./public/css"));
+})
