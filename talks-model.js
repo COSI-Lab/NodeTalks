e2bd8efc397b2c888c0ff12d4001434f4953ab07
Benjamin Lannon <lannonbr@clarkson.edu>
2018-03-11 23:53:40 -0400

    used prettier on talks-model.js file

--- talks-model.js
+++ talks-model.js
@@ -1,14 +1,18 @@
-var sequelize = require('sequelize');
+var sequelize = require("sequelize");
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('talk', {
-		id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-		name: { type: DataTypes.STRING },
-		type: { type: DataTypes.STRING },
-		desc: { type: DataTypes.STRING },
-		hidden: { type: DataTypes.BOOLEAN, defaultValue: false }
-	}, {
-		timestamps: false,
-		tableName: 'talks'
-	});
+  return sequelize.define(
+    "talk",
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      name: { type: DataTypes.STRING },
+      type: { type: DataTypes.STRING },
+      desc: { type: DataTypes.STRING },
+      hidden: { type: DataTypes.BOOLEAN, defaultValue: false }
+    },
+    {
+      timestamps: false,
+      tableName: "talks"
+    }
+  );
 };
